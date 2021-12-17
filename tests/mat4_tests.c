6bb8f028ed917486c3c17516c46549e8db7abbde
Jared <jared@gateway>
2021-12-16 21:36:19 -0500

    Added rotation matrix tests.

--- tests/mat4_tests.c
+++ tests/mat4_tests.c
@@ -20,8 +20,17 @@
  */
 
 #include <assert.h>
+#include <float.h>
 #include <math.h>
 #include <mat4.h>
+
+#define PI                  (3.14159265359)
+#define DEGS2RADS(degs)     (degs * (PI / 180.0))
+#define FLTEQ(a, b)         (fabs(a - b) < FLT_EPSILON)
+
+const float RIGHT[4]   = { 1.0f, 0.0f, 0.0f, 1.0f };
+const float UP[4]      = { 0.0f, 1.0f, 0.0f, 1.0f };
+const float FORWARD[4] = { 0.0f, 0.0f, 1.0f, 1.0f };
 
 MATX(mat4f, _, 4, float, "%f")
 
@@ -29,8 +38,8 @@ MATX_MAT4_HELPERS(mat4f, MAT4F, _, float, sqrtf, sinf, cosf, tanf)
 
 int main()
 {
-        mat4f id, tr;
-        const float pos[3] = { 1.0f, 2.0f, 3.0f };
+        float v[4];
+        mat4f id, tr, rx, ry, rz;
         memcpy(id, MAT4F_IDENT, 16 * sizeof(float));
         printf("================================================================================\n");
         printf("= MAT4F_IDENT test\n");
@@ -45,6 +54,7 @@ int main()
         printf("================================================================================\n");
         printf("= mat4f_transl test\n");
         printf("================================================================================\n");
+        const float pos[3] = { 1.0f, 2.0f, 3.0f };
         mat4f_transl(tr, pos);
         mat4f_printf(tr);
         assert(tr[0][0] == 1.0f && tr[0][1] == 0.0f && tr[0][2] == 0.0f && tr[0][3] == 1.0f &&
@@ -53,6 +63,51 @@ int main()
                tr[3][0] == 0.0f && tr[3][1] == 0.0f && tr[3][2] == 0.0f && tr[3][3] == 1.0f);
         printf("================================================================================\n");
         printf("= mat4f_transl passed\n");
+        printf("================================================================================\n");
+        printf("= mat4f_rotx test\n");
+        printf("================================================================================\n");
+        float xrads = DEGS2RADS(90.0f), xcos = cosf(xrads), xsin = sinf(xrads);
+        mat4f_rotx(rx, xrads);
+        assert(rx[0][0] == 1.0f && rx[0][1] ==  0.0f && rx[0][2] == 0.0f && rx[0][3] == 0.0f &&
+               rx[1][0] == 0.0f && rx[1][1] ==  xcos && rx[1][2] == xsin && rx[1][3] == 0.0f &&
+               rx[2][0] == 0.0f && rx[2][1] == -xsin && rx[2][2] == xcos && rx[2][3] == 0.0f &&
+               rx[3][0] == 0.0f && rx[3][1] ==  0.0f && rx[3][2] == 0.0f && rx[3][3] == 1.0f);
+        mat4f_mulv(v, rx, UP);
+        mat4f_printf(rx);
+        printf("v=%f %f %f %f\n", v[0], v[1], v[2], v[3]);
+        assert(FLTEQ(v[0], 0.0f) && FLTEQ(v[1], -0.0f) && FLTEQ(v[2], -1.0f) && FLTEQ(v[3], 1.0f));
+        printf("================================================================================\n");
+        printf("= mat4f_rotx passed\n");
+        printf("================================================================================\n");
+        printf("= mat4f_roty test\n");
+        printf("================================================================================\n");
+        float yrads = DEGS2RADS(90.0f), ycos = cosf(yrads), ysin = sinf(yrads);
+        mat4f_roty(ry, yrads);
+        printf("v=%f %f %f %f\n", v[0], v[1], v[2], v[3]);
+        assert(ry[0][0] == ycos && ry[0][1] == 0.0f && ry[0][2] == -ysin && ry[0][3] == 0.0f &&
+               ry[1][0] == 0.0f && ry[1][1] == 1.0f && ry[1][2] ==  0.0f && ry[1][3] == 0.0f &&
+               ry[2][0] == ysin && ry[2][1] == 0.0f && ry[2][2] ==  ycos && ry[2][3] == 0.0f &&
+               ry[3][0] == 0.0f && ry[3][1] == 0.0f && ry[3][2] ==  0.0f && ry[3][3] == 1.0f);
+        mat4f_mulv(v, ry, RIGHT);
+        mat4f_printf(ry);
+        assert(FLTEQ(v[0], -0.0f) && FLTEQ(v[1], 0.0f) && FLTEQ(v[2], 1.0f) && FLTEQ(v[3], 1.0f));
+        printf("================================================================================\n");
+        printf("= mat4f_roty passed\n");
+        printf("================================================================================\n");
+        printf("= mat4f_rotz test\n");
+        printf("================================================================================\n");
+        float zrads = DEGS2RADS(90.0f), zcos = cosf(zrads), zsin = sinf(zrads);
+        mat4f_rotz(rz, zrads);
+        mat4f_printf(rz);
+        assert(rz[0][0] == zcos && rz[0][1] == -zsin && rz[0][2] == 0.0f && rz[0][3] == 0.0f &&
+               rz[1][0] == zsin && rz[1][1] ==  zcos && rz[1][2] == 0.0f && rz[1][3] == 0.0f &&
+               rz[2][0] == 0.0f && rz[2][1] ==  0.0f && rz[2][2] == 1.0f && rz[2][3] == 0.0f &&
+               rz[3][0] == 0.0f && rz[3][1] ==  0.0f && rz[3][2] == 0.0f && rz[3][3] == 1.0f);
+        mat4f_mulv(v, rz, UP);
+        printf("v=%f %f %f %f\n", v[0], v[1], v[2], v[3]);
+        assert(FLTEQ(v[0], -1.0f) && FLTEQ(v[1], -0.0f) && FLTEQ(v[2], 0.0f) && FLTEQ(v[3], 1.0f));
+        printf("================================================================================\n");
+        printf("= mat4f_rotz passed\n");
         printf("================================================================================\n");
         return 0;
 }
