d7746225aa93dc77e84e21f42d8facfa5b8970df
Jared B. Resch <jared.resch@hotmail.com>
2022-01-13 23:03:11 -0500

    Formatting.

--- matx.h
+++ matx.h
@@ -32,95 +32,112 @@
 \
 typedef T NAME[N][N]; \
 \
-const SIZE_T CONST ## DIM = N; \
+const SIZE_T CONST ## DIM = N;     \
 const SIZE_T CONST ## SZ  = N * N; \
 \
 MATX_EXPORT void FUN ## init(NAME out, T s) \
 { \
-        for (SIZE_T i = 0; i < N; i++) { \
-                for (SIZE_T j = 0; j < N; j++) out[i][j] = s; \
-        } \
+        SIZE_T i, j;                                   \
+        for (i = 0; i < N; i++) {                      \
+                for (j = 0; j < N; j++) out[i][j] = s; \
+        }                                              \
 } \
 \
 MATX_EXPORT void FUN ## id(NAME out) \
 { \
-        for (SIZE_T i = 0; i < N; i++) { \
-                for (SIZE_T j = 0; j < N; j++) { \
+        SIZE_T i, j;                                        \
+        for (i = 0; i < N; i++) {                           \
+                for (j = 0; j < N; j++) {                   \
                         out[i][j] = (i == j ? (T)1 : (T)0); \
-                } \
-        } \
+                }                                           \
+        }                                                   \
 } \
 \
 MATX_EXPORT BOOL_T FUN ## eq(NAME a, NAME b) \
 { \
-        for (SIZE_T i = 0; i < N; i++) { \
-                for (SIZE_T j = 0; j < N; j++) { \
+        SIZE_T i, j;                                              \
+        for (i = 0; i < N; i++) {                                 \
+                for (j = 0; j < N; j++) {                         \
                         if (a[i][j] != b[i][j]) return (BOOL_T)0; \
-                } \
-        } \
-        return (BOOL_T)1; \
+                }                                                 \
+        }                                                         \
+        return (BOOL_T)1;                                         \
 } \
 \
 MATX_EXPORT BOOL_T FUN ## eqs(NAME m, T s) \
 { \
-        for (SIZE_T i = 0; i < N; i++) { \
-                for (SIZE_T j = 0; j < N; j++) { \
+        SIZE_T i, j;                                        \
+        for (i = 0; i < N; i++) {                           \
+                for (j = 0; j < N; j++) {                   \
                         if (m[i][j] != s) return (BOOL_T)0; \
-                } \
-        } \
-        return (BOOL_T)1; \
+                }                                           \
+        }                                                   \
+        return (BOOL_T)1;                                   \
 } \
 \
 MATX_EXPORT void FUN ## mul(NAME out, NAME a, NAME b) \
 { \
-        NAME tmp; \
-        for (SIZE_T i = 0; i < N; i++) { \
-                for (SIZE_T j = 0; j < N; j++) { \
-                        T sum = (T)0; \
-                        for (SIZE_T k = 0; k < N; k++) { \
+        T sum;                                            \
+        NAME tmp;                                         \
+        SIZE_T i, j, k;                                   \
+        for (i = 0; i < N; i++) {                         \
+                for (j = 0; j < N; j++) {                 \
+                        sum = (T)0;                       \
+                        for (k = 0; k < N; k++) {         \
                                 sum += a[i][k] * b[k][j]; \
-                        } \
-                        tmp[i][j] = sum;\
-                } \
-        } \
-        memcpy(out, tmp, N * N * sizeof(T)); \
+                        }                                 \
+                        tmp[i][j] = sum;                  \
+                }                                         \
+        }                                                 \
+        memcpy(out, tmp, N * N * sizeof(T));              \
 } \
 \
 MATX_EXPORT void FUN ## mulv(T out[N], NAME m, T v[N]) \
 { \
-        T tmp[N]; \
-        for (SIZE_T i = 0; i < N; i++) { \
-                T sum = (T)0; \
-                for (SIZE_T j = 0; j < N; j++) sum += m[i][j] * v[j]; \
-                tmp[i] = sum; \
-        } \
-        memcpy(out, tmp, N * sizeof(T)); \
+        T tmp[N];                                              \
+        SIZE_T i, j;                                           \
+        for (i = 0; i < N; i++) {                              \
+                T sum = (T)0;                                  \
+                for (j = 0; j < N; j++) sum += m[i][j] * v[j]; \
+                tmp[i] = sum;                                  \
+        }                                                      \
+        memcpy(out, tmp, N * sizeof(T));                       \
 } \
 \
 MATX_EXPORT void FUN ## transp(NAME out, NAME m) \
 { \
-        NAME tmp; \
-        for (SIZE_T i = 0; i < N; i++) { \
-                for (SIZE_T j = 0; j < N; j++) tmp[i][j] = m[j][i]; \
-        } \
-        memcpy(out, tmp, N * N * sizeof(T)); \
+        NAME tmp;                                            \
+        SIZE_T i, j;                                         \
+        for (i = 0; i < N; i++) {                            \
+                for (j = 0; j < N; j++) tmp[i][j] = m[j][i]; \
+        }                                                    \
+        memcpy(out, tmp, N * N * sizeof(T));                 \
 } \
 \
 MATX_EXPORT void FUN ## fprintf(FILE *f, NAME m) \
 { \
-        for (SIZE_T i = 0; i < N; i++) { \
-                for (SIZE_T j = 0; j < N; j++) { \
-                        fprintf(f, FMT, m[i][j]); \
+        SIZE_T i, j;                                      \
+        for (i = 0; i < N; i++) {                         \
+                for (j = 0; j < N; j++) {                 \
+                        fprintf(f, FMT, m[i][j]);         \
                         if (j < (N - 1)) fprintf(f, " "); \
-                } \
-                if (i < (N - 1)) fprintf(f, "\n"); \
-        } \
-        fprintf(f, "\n"); \
+                }                                         \
+                if (i < (N - 1)) fprintf(f, "\n");        \
+        }                                                 \
+        fprintf(f, "\n");                                 \
 } \
 \
 MATX_EXPORT void FUN ## printf(NAME m) \
 { \
-        FUN ## fprintf(MATX_STDOUT, m); \
+        SIZE_T i, j;                                  \
+        for (i = 0; i < N; i++) {                     \
+                for (j = 0; j < N; j++) {             \
+                        printf(FMT, m[i][j]);         \
+                        if (j < (N - 1)) printf(" "); \
+                }                                     \
+                if (i < (N - 1)) printf("\n");        \
+        }                                             \
+        printf("\n");                                 \
 }
 
 #endif
